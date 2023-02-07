cfd40407c96d2733197f72c18d94088053dee20a
unionerror <rajatdh27@gmail.com>
2023-02-07 22:33:42 +0530

    Profile start

---
+++ src/Pages/Profile/Profile.jsx
@@ -0,0 +1,40 @@
+import styles from "./Profile.module.css";
+import Feed from "../../components/Feed/Feed";
+import Rightbar from "../../components/Rightbar/Rightbar";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Topbar from "../../components/Topbar/Topbar";
+
+export default function Profile() {
+  return (
+    <>
+      <Topbar />
+      <div className={styles.profile}>
+        <Sidebar />
+        <div className={styles.profileRight}>
+          <div className={styles.profileRightTop}>
+            <div className={styles.profileCover}>
+              <img
+                className={styles.profileCoverImg}
+                src="/assets/post/3.jpeg"
+                alt=""
+              />
+              <img
+                className={styles.profileUserImg}
+                src="/assets/person/4.jpeg"
+                alt=""
+              />
+            </div>
+            <div className={styles.profileInfo}>
+              <h4 className={styles.profileInfoName}>Itachi</h4>
+              <span className={styles.profileInfoDesc}>Hello My Friends</span>
+            </div>
+          </div>
+          <div className={styles.profileRightBottom}>
+            <Feed />
+            <Rightbar profile />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/Rightbar/Rightbar.jsx
+++ src/components/Rightbar/Rightbar.jsx
@@ -2,10 +2,10 @@ import styles from "./Rightbar.module.css";
 import { Users } from "../../dummyData";
 import Online from "../Online/Online";
 
-export default function Rightbar() {
-  return (
-    <div className={styles.rightbar}>
-      <div className={styles.rightbarWrapper}>
+export default function Rightbar({ profile }) {
+  const HomeRightbar = () => {
+    return (
+      <>
         <div className={styles.birthdayContainer}>
           <img className={styles.birthdayImg} src="/assets/gift.png" alt="" />
           <span className={styles.birthdayText}>
@@ -19,6 +19,34 @@ export default function Rightbar() {
             return <Online key={u.id} user={u} />;
           })}
         </ul>
+      </>
+    );
+  };
+  const ProfileRightbar = () => {
+    return (
+      <>
+        <h4 className={styles.rightbarTitle}>User Information</h4>
+        <div className={styles.rightbarInfo}>
+          <div className={styles.rightbarInfoItem}>
+            <span className={styles.rightbarInfoKey}>City: </span>
+            <span className={styles.rightbarInfoKey}>New York</span>
+          </div>
+          <div className={styles.rightbarInfoItem}>
+            <span className={styles.rightbarInfoKey}>From: </span>
+            <span className={styles.rightbarInfoKey}>Madrid</span>
+          </div>
+          <div className={styles.rightbarInfoItem}>
+            <span className={styles.rightbarInfoKey}>Relationship: </span>
+            <span className={styles.rightbarInfoKey}>Single</span>
+          </div>
+        </div>
+      </>
+    );
+  };
+  return (
+    <div className={styles.rightbar}>
+      <div className={styles.rightbarWrapper}>
+        <ProfileRightbar />
       </div>
     </div>
   );
