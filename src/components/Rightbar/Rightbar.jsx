85f51db880962129455e6cbba780443e0b2cb00e
unionerror <rajatdh27@gmail.com>
2023-02-08 19:39:30 +0530

    Profile end

--- src/components/Rightbar/Rightbar.jsx
+++ src/components/Rightbar/Rightbar.jsx
@@ -29,15 +29,58 @@ export default function Rightbar({ profile }) {
         <div className={styles.rightbarInfo}>
           <div className={styles.rightbarInfoItem}>
             <span className={styles.rightbarInfoKey}>City: </span>
-            <span className={styles.rightbarInfoKey}>New York</span>
+            <span className={styles.rightbarInfoValue}>New York</span>
           </div>
           <div className={styles.rightbarInfoItem}>
             <span className={styles.rightbarInfoKey}>From: </span>
-            <span className={styles.rightbarInfoKey}>Madrid</span>
+            <span className={styles.rightbarInfoValue}>Madrid</span>
           </div>
           <div className={styles.rightbarInfoItem}>
             <span className={styles.rightbarInfoKey}>Relationship: </span>
-            <span className={styles.rightbarInfoKey}>Single</span>
+            <span className={styles.rightbarInfoValue}>Single</span>
+          </div>
+        </div>
+        <h4 className={styles.rightbarTitle}>User Friends</h4>
+        <div className={styles.rightbarFollowings}>
+          <div className={styles.rightbarFollowing}>
+            <img
+              className={styles.rightbarFollowingImg}
+              alt=""
+              src="/assets/person/1.jpeg"
+            />
+            <span className={styles.rightbarFollowingName}>John Carter</span>
+          </div>
+          <div className={styles.rightbarFollowing}>
+            <img
+              className={styles.rightbarFollowingImg}
+              alt=""
+              src="/assets/person/2.jpeg"
+            />
+            <span className={styles.rightbarFollowingName}>John Carter</span>
+          </div>
+          <div className={styles.rightbarFollowing}>
+            <img
+              className={styles.rightbarFollowingImg}
+              alt=""
+              src="/assets/person/3.jpeg"
+            />
+            <span className={styles.rightbarFollowingName}>John Carter</span>
+          </div>
+          <div className={styles.rightbarFollowing}>
+            <img
+              className={styles.rightbarFollowingImg}
+              alt=""
+              src="/assets/person/4.jpeg"
+            />
+            <span className={styles.rightbarFollowingName}>John Carter</span>
+          </div>
+          <div className={styles.rightbarFollowing}>
+            <img
+              className={styles.rightbarFollowingImg}
+              alt=""
+              src="/assets/person/5.jpeg"
+            />
+            <span className={styles.rightbarFollowingName}>John Carter</span>
           </div>
         </div>
       </>
