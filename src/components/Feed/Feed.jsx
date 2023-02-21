2064d7ab4e8169b4d81d091fc4b7ab3fc1cc91c9
unionerror <rajatdh27@gmail.com>
2023-02-21 21:57:00 +0530

    first fetch completed

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,16 +1,26 @@
+import { useEffect, useState } from "react";
 import Post from "../Post/Post";
 import Share from "../Share/Share";
 import styles from "./Feed.module.css";
-import { Posts } from "../../dummyData";
+import axios from "axios";
 
 export default function Feed() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("posts/timeline/63eba5021c0a5be3328a5a87");
+      console.log(res);
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, []);
   return (
     <div className={styles.feed}>
       <div className={styles.feedWrapper}>
         <Share />
-        {Posts.map((p) => {
+        {/* {Posts.map((p) => {
           return <Post post={p} key={p.id} />;
-        })}
+        })} */}
       </div>
     </div>
   );
